2b99d4c4c2ba979ad672cb376a18d63803618cb0
Avinash <aviku9911@gmail.com>
2025-04-12 12:12:05 +0530

    Adding a folder of js tutorial

---
+++ namaste js/lecture2.js
@@ -0,0 +1,20 @@
+getName()
+console.log(x)
+console.log(getName);
+confirm.log(getName2);
+
+var x=7;
+
+function getName(){
+    console.log("Namaste JS");
+}
+
+//this will act like a js variable 
+// i.e even after being a function it will store undefineded not the whole function body
+var getName2 = () =>{
+    console.log("Namaste jas")
+}
+
+// console.log(getName);
+// getName()
+// console.log(x)
