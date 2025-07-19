37650a3a02d8897b14e6a8bfd981427dd26701a6
Avinash <aviku9911@gmail.com>
2025-07-19 14:57:07 +0530

    changes in 06_tut and adding 07_tut

---
+++ ReactJS/07_tut/src/Content.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+const Content = () => {
+
+  const [name, setName] = useState("Aman");
+  const [count, setCount] = useState(0);
+
+    return (
+
+        <main>
+          
+        </main>
+    )
+}
+
+export default Content;
